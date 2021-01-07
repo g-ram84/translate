cdeb0d65c31a8f9dca08450918d29c38c6ebceb3
Graeme Chalmers <chalmers.graeme@gmail.com>
2021-01-06 22:17:17 -0800

    added context

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,9 +1,27 @@
 import React from 'react';
+import LanguageContext from '../contexts/LanguageContext';
+import ColorContext from '../contexts/ColorContext';
 
 class Button extends React.Component {
+  renderSubmit(value) {
+    return value === 'english' ? 'Submit' : 'Voorleggen'
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    );
+  }
+
   render() {
     return (
-      <div className="ui button primary">Submit</div>
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
